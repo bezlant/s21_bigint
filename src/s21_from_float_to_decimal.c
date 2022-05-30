560c6be56d1fb6508c51a5d2bca2454dd32e807d
Tartica Rosalba <tarticar@vo-a2.msk.21-school.ru>
2022-05-30 17:19:30 +0300

    start float to decimal implementation

--- src/s21_from_float_to_decimal.c
+++ src/s21_from_float_to_decimal.c
@@ -1,3 +1,7 @@
 #include "s21_decimal.h"
 
-int s21_from_float_to_decimal(float src, s21_decimal *dst) {}
+int s21_from_float_to_decimal(float src, s21_decimal *dst) {
+    float integral;
+    float fractional = modf(src, &integral);
+    set_exponent(dst, 6);
+}

09bc349fe26a1f9da06838fac2c34c3c58b3cd48
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-05-30 18:33:06 +0300

    Finish & test script for testing project

--- docker/test_s21_decimal.sh
+++ docker/test_s21_decimal.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo -e "-------------------------------------------------------------------------------\n"
 echo -e "Style test\n"
 cd ../src
@@ -28,47 +27,25 @@ else
     BUILD_TEST_OUTPUT=-1
 fi;
 printf 'Build test result: %s\n' "$BUILD_TEST_OUTPUT"
+
+echo -e "-------------------------------------------------------------------------------\n"
+echo -e "Functional test\n"
+
+# We are interested only in STDERR. STDOUT is redirected to NULL.
+OUTPUT=$(./test 2>&1 > /dev/null)
+
+printf 'Functional test output:\n %s\n' "$OUTPUT"
+
+if [ -z "$OUTPUT" ]; then
+    FUNCTIONAL_TEST_OUTPUT=0
+else
+    FUNCTIONAL_TEST_OUTPUT=-1
+fi;
+printf 'Functional test result: %s\n' "$FUNCTIONAL_TEST_OUTPUT"
 echo -e "-------------------------------------------------------------------------------\n"
 
-# RUN "src/make 2>build_report_errors.txt > build_report_stdout.txt"
-
-# RUN "src/make > build_report.txt"
-
-# RUN "src/make 2>build_report_errors.txt > build_report_stdout.txt"
-
-# launch test executable
-# chmod +x ../src/test
-# ../src/test
-
-# if ../src/test; then
-#     
-#     echo command returned true
-# else
-#     echo command returned some error
-# fi
-
-# command
-# status=$?
-# ## run date command ##
-# cmd="date"
-# $cmd
-# ## get status ##
-# status=$?
-# ## take some decision ## 
-# [ $status -eq 0 ] && echo "$cmd command was successful" || echo "$cmd failed"
-
-# set -e
-# BUILD_SCRIPT=docker/build.sh
-# OUTPUT=$(bash ${BUILD_SCRIPT} part1)
-# printf 'Build output:\n %s\n' "$OUTPUT"
-# BUILD_RESULT=${OUTPUT: -1}
-# printf 'Build result: %s\n' "$BUILD_RESULT"
-# echo -e "-------------------------------------------------------------------------------\n"
-# echo -e "\n"
-# FUNCTIONAL_SCRIPT=docker/functional.sh
-# OUTPUT=$(bash ${BUILD_SCRIPT} part1)
-# printf 'Build output:\n %s\n' "$OUTPUT"
-# BUILD_RESULT=${OUTPUT: -1}
-# printf 'Build result: %s\n' "$BUILD_RESULT"
-# echo -e "-------------------------------------------------------------------------------\n"
-# echo -e "\n"
+if [[ $FUNCTIONAL_TEST_OUTPUT -eq 0 && $BUILD_TEST_OUTPUT -eq 0 && $STYLE_TEST_RESULT -eq 0 ]]; then
+    exit 0
+else
+    exit 1
+fi;

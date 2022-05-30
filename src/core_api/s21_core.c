2229344f69dcd468d504bbbbd87cfc1c5fc22833
AndreyZhelannikov <zhelannikovAndrey@gmail.com>
2022-05-30 22:47:29 +0300

    returning the latest commits

--- src/core_api/s21_core.c
+++ src/core_api/s21_core.c
@@ -26,22 +26,36 @@ void init_zero(s21_decimal *n) {
     INIT_ZERO(n->bits[2]);
 }
 
-void bit_xor(s21_decimal a, s21_decimal b, s21_decimal *result) {
-    result->bits[0] = a.bits[0] ^ b.bits[0];
-    result->bits[1] = a.bits[1] ^ b.bits[1];
-    result->bits[2] = a.bits[2] ^ b.bits[2];
+s21_decimal bit_xor(s21_decimal a, s21_decimal b) {
+    s21_decimal result;
+    result.bits[0] = a.bits[0] ^ b.bits[0];
+    result.bits[1] = a.bits[1] ^ b.bits[1];
+    result.bits[2] = a.bits[2] ^ b.bits[2];
+    return result;
 }
 
-void bit_and(s21_decimal a, s21_decimal b, s21_decimal *result) {
-    result->bits[0] = a.bits[0] & b.bits[0];
-    result->bits[1] = a.bits[1] & b.bits[1];
-    result->bits[2] = a.bits[2] & b.bits[2];
+s21_decimal bit_and(s21_decimal a, s21_decimal b) {
+    s21_decimal result;
+    result.bits[0] = a.bits[0] & b.bits[0];
+    result.bits[1] = a.bits[1] & b.bits[1];
+    result.bits[2] = a.bits[2] & b.bits[2];
+    return result;
 }
 
-void bit_or(s21_decimal a, s21_decimal b, s21_decimal *result) {
-    result->bits[0] = a.bits[0] | b.bits[0];
-    result->bits[1] = a.bits[1] | b.bits[1];
-    result->bits[2] = a.bits[2] | b.bits[2];
+s21_decimal bit_not(s21_decimal a) {
+    s21_decimal result;
+    result.bits[0] = ~a.bits[0];
+    result.bits[1] = ~a.bits[1];
+    result.bits[2] = ~a.bits[2];
+    return result;
+}
+
+s21_decimal bit_or(s21_decimal a, s21_decimal b) {
+    s21_decimal result;
+    result.bits[0] = a.bits[0] | b.bits[0];
+    result.bits[1] = a.bits[1] | b.bits[1];
+    result.bits[2] = a.bits[2] | b.bits[2];
+    return result;
 }
 
 void shift_l_one(s21_decimal *a) {
@@ -87,6 +101,9 @@ void set_exponent(s21_decimal *decimal, int new_exponent) {
         fprintf(stderr, "VERY BIG EXPONENT (0 - 28): exp = %d\n", new_exponent);
     }
 }
+
+int max(int a, int b) { return a > b ? a : b; }
+int min(int a, int b) { return a < b ? a : b; }
 
 // void print_bits(s21_decimal d) {
 //     for (int i = 0; i < 3; i++) {
--- src/math_ops/s21_add.c
+++ src/math_ops/s21_add.c
@@ -1,7 +1,10 @@
 #include "../s21_decimal.h"
 
 s21_decimal binary_addition(s21_decimal value_1, s21_decimal value_2);
-handle_exponent(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+void handle_exponent(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+s21_decimal get_power_of_ten(int pow);
+s21_decimal binary_subtraction(s21_decimal value_1, s21_decimal value_2);
+s21_decimal binary_multiplication(s21_decimal value_1, s21_decimal value_2);
 #define DEBUG
 
 #ifdef DEBUG
@@ -9,101 +12,77 @@ void print_bits(s21_decimal d);
 #endif
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-    // init_zero(result);
-    // for (int i = 0; i < 96; ++i) {
-    // }
-
     if (get_sign(value_1) == get_sign(value_2)) {
-        // *result = binary_addition(value_1, value_2);
         handle_exponent(value_1, value_2, result);
     } else {
-        //    s21_sub(value_1, value_2, result);
+        // s21_sub(value_1, value_2, result);
     }
 }
 
-handle_exponent(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+void handle_exponent(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     int exp_v1 = get_exponent(value_1);
     int exp_v2 = get_exponent(value_2);
-    *result = value_1;
-    if (exp_v1 >= exp_v2) {
-        for (int i = exp_v1; i <= exp_v2; ++i) {
-            *result = binary_addition(*result, value_2);
-        }
+    s21_decimal power_of_ten = get_power_of_ten(abs(exp_v1 - exp_v2));
+    if (exp_v1 > exp_v2) {
+        value_2 = binary_multiplication(value_2, power_of_ten);
+        *result = binary_addition(value_2, value_1);
     } else {
-        for (int i = exp_v2; i <= exp_v1; ++i) {
-            *result = binary_addition(*result, value_2);
-        }
+        value_1 = binary_multiplication(value_1, power_of_ten);
+        *result = binary_addition(value_2, value_1);
     }
+    set_exponent(result, max(exp_v1, exp_v2));
 }
 
 s21_decimal binary_addition(s21_decimal value_1, s21_decimal value_2) {
     s21_decimal carry;
     while (!eq_zero(value_2)) {
         init_zero(&carry);
-        bit_and(value_1, value_2, &carry);
+        carry = bit_and(value_1, value_2);
         shift_l_one(&carry);
-        bit_xor(value_1, value_2, &value_1);
+        value_1 = bit_xor(value_1, value_2);
         value_2 = carry;
     }
     return value_1;
 }
 
-int eq_zero(s21_decimal value) { return (value.bits[0] == 0 && value.bits[1] == 0 && value.bits[2] == 0); }
+s21_decimal binary_subtraction(s21_decimal value_1, s21_decimal value_2) {
+    s21_decimal carry;
+    value_2 = binary_addition(bit_not(value_2), get_power_of_ten(0));
+    while (!eq_zero(value_2)) {
+        init_zero(&carry);
+        carry = bit_and(value_1, value_2);
+        shift_l_one(&carry);
+        value_1 = bit_xor(value_1, value_2);
+        value_2 = carry;
+    }
+    return value_1;
+}
 
-// int s21_add(s21_decimal a, s21_decimal b) {
-//     s21_decimal res = {0};
-//     // a negative b positive
-//     if (get_sign(a) && !get_sign(b)) {
-//         res = s21_sub(b, a);
-//         // a positive b negative
-//     } else if (!get_sign(a) && get_sign(b)) {
-//         res = s21_sub(a, b);
-//         // both positive
-//     } else {
-//         res = add(a, b);
-//     }
-//     return res;
-// }
-//
-// int add(s21_decimal a, s21_decimal b) {
-//     // FIXES NEEDED! TURNS OUT '.' CAN BE AT ANY POSITION UP TO 28
-//     s21_decimal res = {0};
-//     if (!get_exponent(a) && !get_exponent(b))
-//         res = add_integers(a, b);
-//     else
-//         res = add_floats(a, b);
-//     return res;
-// }
-//
-//// msb = most significant bit
-// int check_overflow(s21_decimal val, int msb) {
-//     int res = IS_SET(val.bits[0], msb);
-//     return res;
-// }
-//
-// s21_decimal add_integers(s21_decimal a, s21_decimal b) {
-//     s21_decimal res = {0};
-//     unsigned int *b1 = a.bits;
-//     unsigned int *b2 = b.bits;
-//     bool cf = false;
-//     for (int i = 0; i < 3; i++) {
-//         for (int j = 0; j < 32; j++) {
-//             int current = cf + IS_SET(b1[i], j) + IS_SET(b2[i], j);
-//             if (current == 3) {
-//                 ADD_BIT(res.bits[i], j);
-//                 cf = true;
-//             }
-//             if (current == 2) {
-//                 cf = true;
-//             }
-//             if (current == 1) {
-//                 ADD_BIT(res.bits[i], j);
-//                 cf = false;
-//             }
-//         }
-//         if (cf)
-//             return S21_INFINITY;
-//     }
-//     return res;
-// }
-//
+s21_decimal binary_multiplication(s21_decimal value_1, s21_decimal value_2) {
+    s21_decimal result;
+    init_zero(&result);
+    while (!eq_zero(value_2)) {
+        if (!eq_zero(bit_and(value_2, get_power_of_ten(0)))) {
+            result = binary_addition(result, value_1);
+        }
+        shift_l_one(&value_1);
+        shift_r_one(&value_2);
+    }
+    return result;
+}
+
+s21_decimal get_power_of_ten(int pow) {
+    s21_decimal result;
+    init_zero(&result);
+    set_exponent(&result, 0);
+    for (int i = 0; i < 96; ++i) {
+        if (binary_powers_of_ten[pow][95 - i] == '1') {
+            set_bit_1(&result, i);
+        } else {
+            set_bit_0(&result, i);
+        }
+    }
+    return result;
+}
+
+int eq_zero(s21_decimal value) { return (value.bits[0] == 0 && value.bits[1] == 0 && value.bits[2] == 0); }

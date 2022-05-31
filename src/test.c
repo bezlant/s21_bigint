af45a9b564bdc87f50c0256046eb9a4299ababee
capricey <zhelannikovandrey@gmail.com>
2022-05-31 09:59:33 +0300

    gmp lib

--- src/test.c
+++ src/test.c
@@ -1,42 +1,14 @@
-#include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/*
-** Using documented GCC type unsigned __int128 instead of undocumented
-** obsolescent typedef name __uint128_t.  Works with GCC 4.7.1 but not
-** GCC 4.1.2 (but __uint128_t works with GCC 4.1.2) on Mac OS X 10.7.4.
-*/
-typedef unsigned __int128 uint128_t;
+#include "gmp.h"
 
-/*      UINT64_MAX 18446744073709551615ULL */
-#define P10_UINT64 10000000000000000000ULL /* 19 zeroes */
-#define E10_UINT64 19
-
-#define STRINGIZER(x) #x
-#define TO_STRING(x) STRINGIZER(x)
-
-static int print_u128_u(uint128_t u128) {
-    int rc;
-    if (u128 > UINT64_MAX) {
-        uint128_t leading = u128 / P10_UINT64;
-        uint64_t trailing = u128 % P10_UINT64;
-        rc = print_u128_u(leading);
-        rc += printf("%." TO_STRING(E10_UINT64) PRIu64, trailing);
-    } else {
-        uint64_t u64 = u128;
-        rc = printf("%" PRIu64, u64);
-    }
-    return rc;
+void foo() {
+    mpq_t quotient;
+    //
 }
 
 int main(void) {
-    uint128_t u128a = 79228162514264337593543950335;
-    ;
-    for (int i = 0; i < 100; i++) {
-        printf("%d : ", i);
-        int n = print_u128_u(u128a);
-        printf(": %d\n", n);
-        u128a *= 2;
-    }
-    return (0);
+    foo();
+    return 0;
 }

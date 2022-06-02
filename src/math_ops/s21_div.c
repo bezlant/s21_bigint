1e95ecae7e67ec70cd00257718c0d44aa5918c72
Caprice Ytrams <capricey@su-a2.msk.21-school.ru>
2022-06-02 16:39:35 +0300

    mul + div

--- src/math_ops/s21_div.c
+++ src/math_ops/s21_div.c
@@ -32,13 +32,15 @@ void handle_exponent_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *
 }
 
 s21_decimal binary_division(s21_decimal value_1, s21_decimal value_2, int *code) {
-    if (s21_is_equal(value_2, get_power_of_ten(0))) return value_1;
-
     s21_decimal result = {0};
-    for (int i = 95 - byte_len(value_2); i >= 0; --i) {
-        if (s21_is_less_or_equal(shiftnl_ret(value_2, i), value_1)) {
-            value_1 = binary_subtraction(value_1, shiftnl_ret(value_2, i), code);
-            result = bit_or(result, shiftnl_ret(get_power_of_ten(0), i));
+    if (s21_is_equal(value_2, get_power_of_ten(0))) {
+        result = value_1;
+    } else {
+        for (int i = 95 - byte_len(value_2); i >= 0; --i) {
+            if (s21_is_less_or_equal(shiftnl_ret(value_2, i), value_1)) {
+                value_1 = binary_subtraction(value_1, shiftnl_ret(value_2, i), code);
+                result = bit_or(result, shiftnl_ret(get_power_of_ten(0), i));
+            }
         }
     }
     return result;

deac58fd7a67dbb6b3dda2eb701c391cfd868aac
bezlant <89563512+bezlant@users.noreply.github.com>
2022-06-03 11:26:41 +0300

    added simple tests for division (#16)
    Co-authored-by: Tartica Rosalba <tarticar@vo-a2.msk.21-school.ru>

--- src/tests/test_helpers.c
+++ src/tests/test_helpers.c
@@ -7,27 +7,30 @@ s21_decimal convert_gmp_to_decimal(mpz_t input) {
     return res;
 }
 
-int get_random_pair(s21_decimal *in, mpz_t *in_mpz_copy) {
-    if (rand() % 2)
+int get_random_pair(s21_decimal *in, mpz_t *in_mpz_copy, int size) {
+    /* NOTE: Negatives disabled for now */
+    if (0)
         set_sign_neg(in);
     else
         set_sign_pos(in);
 
-    char mpz_bin_str[1024] = {0};
-    for (int j = 0, m = 0; j < 1; j++)
+    char mpz_bin_str[96] = {0};
+    for (int j = 0, m = 0; j < size; j++)
         for (int i = 0; i < 32; i++, m++) {
             if (rand() % 2) {
                 ADD_BIT(in->bits[j], i);
                 mpz_bin_str[m] = '1';
             } else {
+                ZERO_BIT(in->bits[j], i);
                 mpz_bin_str[m] = '0';
             }
         }
 
-    for (int j = 0; j < 3; j++) in->bits[j] = reverse_bits(in->bits[j]);
+    for (int j = 0; j < 3; j++)
+        in->bits[j] = reverse_bits(in->bits[j]);
 
     /* TODO: Changes needed */
-    int exp = get_rand(3, 5);
+    int exp = get_rand(0, 0);
     set_exponent(in, exp);
 
     bool res = mpz_set_str(*in_mpz_copy, mpz_bin_str, 2);
@@ -87,11 +90,21 @@ s21_decimal get_random_int_decimal(void) {
     return res;
 }
 
+void print_mpz_binary(mpz_t mpz_val) {
+    mpz_out_str(stdout, 2, mpz_val);
+    puts("");
+}
+void print_mpz_decimal(mpz_t mpz_val) {
+    mpz_out_str(stdout, 10, mpz_val);
+    puts("");
+}
+
 void print_bits(s21_decimal d) {
     for (int i = 0; i < 4; i++) {
         int x = 3;
         for (int j = 0; j < 32; j++) {
-            if (IS_SET(d.bits[i], j)) printf("%s", HRED);
+            if (IS_SET(d.bits[i], j))
+                printf("%s", HRED);
             printf("%u", IS_SET(d.bits[i], j));
             if (j == x && !(i == 2 && j == 31)) {
                 x += 4;
@@ -105,10 +118,12 @@ void print_bits(s21_decimal d) {
 
 void print_bits_r(s21_decimal d) {
     for (int i = 3; i >= 0; --i) {
-        if (i == 2) printf(" ");
+        if (i == 2)
+            printf(" ");
         int x = 28;
         for (int j = 31; j >= 0; --j) {
-            if (IS_SET(d.bits[i], j)) printf("%s", HRED);
+            if (IS_SET(d.bits[i], j))
+                printf("%s", HRED);
             printf("%u", IS_SET(d.bits[i], j));
             if (j == x && !(i == 0 && j == 0)) {
                 x -= 4;
@@ -123,7 +138,8 @@ void print_bits_r(s21_decimal d) {
 void print_bits_r_set(s21_decimal d, int set_n) {
     int x = 28;
     for (int j = 15; j >= 0; --j) {
-        if (IS_SET(d.bits[set_n], j)) printf("%s", HRED);
+        if (IS_SET(d.bits[set_n], j))
+            printf("%s", HRED);
         printf("%u", IS_SET(d.bits[set_n], j));
         if (j == x && !(set_n == 0 && j == 0)) {
             x -= 4;
@@ -138,7 +154,8 @@ void print_bits_r_set(s21_decimal d, int set_n) {
 void print_bits_set(s21_decimal d, int set_n) {
     int x = 28;
     for (int j = 0; j < 16; ++j) {
-        if (IS_SET(d.bits[set_n], j)) printf("%s", HRED);
+        if (IS_SET(d.bits[set_n], j))
+            printf("%s", HRED);
         printf("%u", IS_SET(d.bits[set_n], j));
         if (j == x && !(set_n == 0 && j == 0)) {
             x -= 4;

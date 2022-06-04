b96d8fcc2738940ad7195e4efed464aaec1fea2c
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-06-04 04:56:07 +0300

    [QA] Create basic version of tests for comparison

--- src/tests/comparison_tests/test_s21_is_equal_not_equal.c
+++ src/tests/comparison_tests/test_s21_is_equal_not_equal.c
@@ -28,6 +28,48 @@ START_TEST(gmp_random) {
 }
 END_TEST
 
+START_TEST(gmp_random_inequality) {
+    int inequality = rand() % 2;
+    s21_decimal a1 = {0};
+    init_zero(&a1);
+
+    mpz_t mpz_val, mpz_copy;
+    mpz_init(mpz_val);
+    mpz_init(mpz_copy);
+    mpz_set_ui(mpz_val, 0);
+    mpz_set_ui(mpz_copy, 0);
+
+    get_random_pair(&a1, &mpz_val, 3);
+
+    convert_decimal_to_gmp(&mpz_copy, &a1);
+    s21_decimal res = convert_gmp_to_decimal(mpz_val);
+
+    if (inequality) {
+        a1.bits[0] = rand();
+        a1.bits[1] = rand();
+        get_random_pair(&a1, &mpz_val, 3);
+        a1.bits[2] = rand();
+        res.bits[0] = -3;
+
+        mpz_clear(mpz_copy);
+        mpz_clear(mpz_val);
+
+        ck_assert_int_eq(s21_is_not_equal(res, a1), 1);
+    } else {
+        /* (!) Hack. Change this to our own function */
+        tmp_normalize_exponent(&a1);
+
+        int expected = mpz_cmp(mpz_val, mpz_copy);
+        int got = !(s21_is_equal(a1, res));
+
+        mpz_clear(mpz_copy);
+        mpz_clear(mpz_val);
+
+        ck_assert_int_eq(got, abs(expected));
+    }
+}
+END_TEST
+
 START_TEST(equality_all_zeros) {
     s21_decimal n1 = {0};
     s21_decimal n2 = {0};
@@ -36,7 +78,6 @@ START_TEST(equality_all_zeros) {
     s21_decimal n3 = {0};
     s21_decimal n4 = {0};
     set_exponent(&n3, 5);
-
 
     s21_decimal n5 = {0};
     set_sign_pos(&n5);
@@ -58,14 +99,16 @@ START_TEST(equality_all_zeros) {
 }
 END_TEST
 
-Suite *suite_s21_is_equal(void) {
-    Suite *s = suite_create(PRETTY_PRINT("s21_is_equal"));
-    TCase *tc = tcase_create("s21_is_equal_tc");
+Suite *suite_s21_is_or_not_equal(void) {
+    Suite *s = suite_create(PRETTY_PRINT("s21_is_or_not_equal"));
+    TCase *tc = tcase_create("s21_is_or_not_equal_tc");
 
     /* Tests: (1) sign, (2) exponent (0 - 28), (3) numbers */
     tcase_add_loop_test(tc, gmp_random, 0, 100);
     /* All zeroes are the only possible special case here */
     tcase_add_test(tc, equality_all_zeros);
+    /* Randomly tests inequality in ~50% of cases */
+    tcase_add_loop_test(tc, gmp_random_inequality, 0, 100);
 
     suite_add_tcase(s, tc);
     return s;
--- src/tests/comparison_tests/test_s21_is_less_or_equal.c
+++ src/tests/comparison_tests/test_s21_is_less_or_equal.c
@@ -1,11 +1,50 @@
 #include "../s21_decimal_test.h"
 
+START_TEST(gmp_random) {
+    s21_decimal a1 = {0};
+    init_zero(&a1);
+
+    mpz_t mpz_val, mpz_copy;
+    mpz_init(mpz_val);
+    mpz_init(mpz_copy);
+    mpz_set_ui(mpz_val, 0);
+    mpz_set_ui(mpz_copy, 0);
+
+    get_random_pair(&a1, &mpz_val, 3);
+
+    convert_decimal_to_gmp(&mpz_copy, &a1);
+    s21_decimal res = convert_gmp_to_decimal(mpz_val);
+
+    /* Produces randomly smaller / greater numbers */
+    if (rand() % 2)
+        set_exponent(&a1, (get_exponent(a1) + 1) % 28);
+
+    /* (!) Hack. Change this to our own function */
+    tmp_normalize_exponent(&a1);
+
+    // Compare op1 and op2. Return a positive value if op1 > op2, zero if op1 = op2, or a negative value if op1 < op2.
+    int got1 = (s21_is_less_or_equal(a1, res));
+    int got2 = (s21_is_less_or_equal(a1, res));
+    int expected1 = mpz_cmp(mpz_val, mpz_copy);
+    int expected2 = mpz_cmp(mpz_val, mpz_copy);
+
+    mpz_clear(mpz_copy);
+    mpz_clear(mpz_val);
+
+    if (got1 == 1 && (expected1 == 0 || expected2 == -1)) {
+        /* Test is passed */
+        ck_assert_int_eq(1, 1);
+    } else {
+        ck_assert_int_eq(got1, expected1 + expected2);
+    }
+}
+END_TEST
 
 Suite *suite_s21_is_less_or_equal(void) {
-    Suite *s = suite_create("suite_s21_is_less_or_equal");
+    Suite *s = suite_create(PRETTY_PRINT("s21_is_less_or_equal"));
     TCase *tc = tcase_create("s21_is_less_or_equal_tc");
 
-    // tcase_add_test(tc, );
+    tcase_add_loop_test(tc, gmp_random, 0, 100);
 
     suite_add_tcase(s, tc);
     return s;

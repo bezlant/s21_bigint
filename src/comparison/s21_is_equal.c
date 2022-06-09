64fe36dccd23a29e917a75ab77757f5b8f9de719
bezlant <89563512+bezlant@users.noreply.github.com>
2022-06-09 13:28:02 +0300

    [FEATURE] Tests added for add, div, sub (#35)
    * Added tests using builtin int128 type for add, sub, div
    
    * added missing functions
    
    Co-authored-by: Tartica Rosalba <tarticar@vo-a2.msk.21-school.ru>

--- src/comparison/s21_is_equal.c
+++ src/comparison/s21_is_equal.c
@@ -1,21 +1,22 @@
 #include "../s21_decimal.h"
 
-bool bits_eq(const unsigned int bits_a[3], const unsigned int bits_b[3]) {
+static bool bits_eq(const unsigned int bits_a[4],
+                    const unsigned int bits_b[4]) {
     return bits_a[0] == bits_b[0] && bits_a[1] == bits_b[1] &&
            bits_a[2] == bits_b[2] && bits_a[3] == bits_b[3];
 }
 
 int s21_is_equal(s21_decimal a, s21_decimal b) {
-    return (both_all_zeroes(a, b)) ? true : bits_eq(a.bits, b.bits);
+    return (both_all_zeroes(a, b)) ? TRUE : bits_eq(a.bits, b.bits);
 }
 
 bool both_all_zeroes(s21_decimal a, s21_decimal b) {
-    int res = true;
+    int res = TRUE;
 
     for (int i = 0; i < 3 && res; i++) {
         for (int j = 0; j < 32; j++) {
             if (IS_SET(a.bits[i], j) || IS_SET(b.bits[i], j)) {
-                res = false;
+                res = FALSE;
                 break;
             }
         }

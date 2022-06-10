6db4218a6e151f6637a945faab2b8083900ef3bd
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-06-10 17:47:37 +0300

    [Fix] Remove deprec. API

--- src/conversion/s21_from_int_to_decimal.c
+++ src/conversion/s21_from_int_to_decimal.c
@@ -1,9 +1,10 @@
+
 #include "../s21_decimal.h"
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
     convertation_result status = CONVERTATION_OK;
     if (dst) {
-        init_decimal(dst);
+        memset(dst, 0, sizeof(unsigned int) * 4);
 
         if (src >= 0) {
             dst->bits[0] = src;
@@ -14,5 +15,6 @@ int s21_from_int_to_decimal(int src, s21_decimal *dst) {
     } else {
         status = CONVERTATION_ERROR;
     }
+
     return status;
 }

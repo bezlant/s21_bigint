0553ab7e42540fb3e94b4b9e39285375a4af6d52
bezlant <89563512+bezlant@users.noreply.github.com>
2022-06-12 11:22:33 +0300

    [FIXES] Floor & Truncate implemented (Documentation added) (#46)
    * Reimplemented truncate (works, needs div)
    
    * Reimplemented floor (works, needs truncate)
    
    * Updated test runner, added notes
    
    * Documentation added for all functions & tests
    
    * [!] Tests still need to be implemented
    
    * [!] Removed old code. Round is NOT implemented.

--- src/rounding/s21_floor.c
+++ src/rounding/s21_floor.c
@@ -1,42 +1,40 @@
 #include "../s21_decimal.h"
 
+/**
+ * @brief Round down the given number
+ * Simply truncated the number (will discard zeroes)
+ * (exp = 7) 51351.5315153 becomes 51351
+ * Then multiply by the pow(10, exponent)
+ * (exp = 7) 51351 becomes 51351.0000000
+ *
+ * For negatives we add one (
+ * i.e -1321.14121 becomes -1322.00000
+ * )
+ *
+ * @param value given value
+ * @param result truncated value
+ * @return returns an error code
+ */
+
 int s21_floor(s21_decimal value, s21_decimal *result) {
     memset(result, 0, sizeof(*result));
-    float res = 0;
-    double exp = get_exponent(value);
-    s21_from_decimal_to_float(value, &res);
+    int exp = get_exponent(value);
+    int sign = get_sign(value);
 
-#ifdef DEBUG
-    printf("before=%f\n", res);
-#endif
+    s21_decimal truncated = {0};
+    s21_truncate(value, &truncated);
 
-    res = floorf(res);
+    /* Round down for negative numbers -12.321351 becomes -13.00000 */
+    if (sign == NEG) {
+        s21_decimal tmp = {0};
+        if (s21_add(truncated, get_power_of_ten(0), &tmp))
+            return CONVERTATION_ERROR;
+        truncated = tmp;
+    }
 
-#ifdef DEBUG
-    printf("after=%f\n", res);
-#endif
+    if (s21_mul(truncated, get_power_of_ten(exp), result))
+        return CONVERTATION_ERROR;
 
-    res = res > 0 ? res : -res;
-
-    /* NOTE: this is a try to make exponent work (doesn't work, loses precision)
-     */
-    /* res = add_exponent(res, get_exponent(value)); */
-    /* printf("after add_exponent=%f\n", res); */
-
-    get_sign(value) ? set_sign_neg(result) : set_sign_pos(result);
-
-    /* handle left part */
-    char bits[96] = {'\0'};
-    get_bit_string(res, bits);
-
-    /* set the bits from string */
-    set_bits_from_string(bits, result);
-
-    /* NOTE: this doesn't work (always give 0 back) */
-    s21_decimal tmp = {0};
-    s21_mul(*result, get_power_of_ten(exp), &tmp);
-
-    *result = tmp;
     set_exponent(result, exp);
 
     return CONVERTATION_OK;
--- src/tests/rounding_tests/test_s21_floor.c
+++ src/tests/rounding_tests/test_s21_floor.c
@@ -1,20 +1,23 @@
 #include "../s21_decimal_test.h"
 
+/* TODO: Implement tests using gmp(suicide) */
+/* simply dividing by 10^exp to truncate & multiplying by 10^exp afterwards*/
+/* i.e 764183413481 / 10000 = 76418341 */
+
 START_TEST(simple_floor) {
     s21_decimal value = get_random_decimal(1, 5);
     s21_decimal res = {0};
 
-    float flt;
-    s21_from_decimal_to_float(value, &flt);
-    printf("flt=%f\n", flt);
+    int code = s21_floor(value, &res);
 
-    s21_floor(value, &res);
-
+#ifdef DEBUG
     printf("before=");
     print_bits_r(value);
-
     printf("after= ");
     print_bits_r(res);
+#endif
+
+    ck_assert_int_eq(code, CONVERTATION_OK);
 }
 END_TEST
 
@@ -22,7 +25,7 @@ Suite *suite_s21_floor(void) {
     Suite *s = suite_create("suite_s21_floor");
     TCase *tc = tcase_create("s21_floor_tc");
 
-    tcase_add_test(tc, simple_floor);
+    tcase_add_loop_test(tc, simple_floor, 0, 1);
 
     suite_add_tcase(s, tc);
     return s;

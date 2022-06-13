4fc105267d3ec07b04bffe578abd42662c1e2780
bezlant <abezludniy@mail.ru>
2022-06-13 11:32:11 +0300

    improved code readability

--- src/conversion/s21_negate.c
+++ src/conversion/s21_negate.c
@@ -4,10 +4,7 @@ int s21_negate(s21_decimal value, s21_decimal *result) {
     for (int i = 0; i < 4; i++)
         result->bits[i] = value.bits[i];
 
-    if (value.bits[3] >> 31) {
-        ZERO_BIT(result->bits[3], 31);
-    } else {
-        ADD_BIT(result->bits[3], 31);
-    }
+    set_sign(result, get_sign(value));
+
     return CONVERTATION_OK;
 }

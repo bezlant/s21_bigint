284d18e7d094284e0bd878a86a22e59bcec24075
bezlant <abezludniy@mail.ru>
2022-06-13 08:27:31 +0300

    All tests are implemented using int128_t [Works well]

--- src/tests/rounding_tests/test_s21_truncate.c
+++ src/tests/rounding_tests/test_s21_truncate.c
@@ -1,24 +1,35 @@
 #include "../s21_decimal_test.h"
 
-/* TODO: Implement tests using gmp(suicide) */
-/* simply dividing by 10 to truncate */
-/* i.e 764183413481 / 10000 = 76418341 */
+START_TEST(gcc_128_bits) {
+    /* get random value */
+    __int128_t a = llabs(get_random_ll() * rand());
 
-START_TEST(simple_truncate) {
-    s21_decimal value = get_random_decimal(3, 28);
+    /* apply exponent */
+    int exp = get_rand(0, 10);
+    __int128_t b = int128_pow(10, exp);
+    __int128_t sum = a / b;
+
+    /* get expected */
+    s21_decimal expected = bigint_to_decimal(sum);
+
+    s21_decimal got = bigint_to_decimal(a);
+    set_exponent(&got, exp);
+
     s21_decimal res = {0};
 
-    int ret = s21_truncate(value, &res);
+    int ret = s21_truncate(got, &res);
 
 #ifdef DEBUG
     printf("before=");
-    print_bits_r(value);
-    printf("after= ");
+    print_bits_r(got);
+    printf("expected=");
+    print_bits_r(expected);
+    printf("res_dec =");
     print_bits_r(res);
 #endif
 
     /* HACK: res == res */
-    ck_assert_float_eq(s21_is_equal(res, res), TRUE);
+    ck_assert_float_eq(s21_is_equal(expected, res), TRUE);
     ck_assert_int_eq(ret, CONVERTATION_OK);
 }
 END_TEST
@@ -27,7 +38,7 @@ Suite *suite_s21_truncate(void) {
     Suite *s = suite_create("suite_s21_truncate");
     TCase *tc = tcase_create("s21_truncate_tc");
 
-    tcase_add_loop_test(tc, simple_truncate, 0, 1);
+    tcase_add_loop_test(tc, gcc_128_bits, 0, 10000);
 
     suite_add_tcase(s, tc);
     return s;

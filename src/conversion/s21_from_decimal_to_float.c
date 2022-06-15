a34db5453a8f2e59e5ce627f7737338bcd5696c2
bezlant <abezludniy@mail.ru>
2022-06-15 09:30:35 +0300

    Stable version of converters [Tests needed]

--- src/conversion/s21_from_decimal_to_float.c
+++ src/conversion/s21_from_decimal_to_float.c
@@ -5,13 +5,16 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst) {
     if (!dst)
         return CONVERTATION_ERROR;
 
-    int exp = get_exponent(src);
+    int exponent = get_exponent(src);
 
     *dst = 0;
     for (int i = 0; i < 96; i++)
         *dst += pow(2, i) * get_bit(src, i);
 
-    while (exp--)
+    /* 29 is max values in our number */
+    exponent = 29 - exponent;
+
+    while (exponent--)
         *dst /= 10;
 
     if (sign)

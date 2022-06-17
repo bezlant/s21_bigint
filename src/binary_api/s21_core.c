f65386754cecaf4e8e6d80a61f82e0819e1f27b9
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-06-17 13:23:57 +0300

    [WIP]

--- src/binary_api/s21_core.c
+++ src/binary_api/s21_core.c
@@ -30,8 +30,6 @@ void s21_swap(s21_decimal *a, s21_decimal *b) {
     *b = tmp;
 }
 
-
-
 /**
  * @brief This function is used to generate 0,5 decimal value that is
  * needed by bank rounding.
@@ -39,7 +37,7 @@ void s21_swap(s21_decimal *a, s21_decimal *b) {
  * @return s21_decimal
  */
 
- s21_decimal get_05(void) {
+s21_decimal get_05(void) {
     s21_decimal result;
     s21_from_int_to_decimal(5, &result);
     set_exponent(&result, 1);

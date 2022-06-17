1d06fa3c52fa0088c5a1c2356c9f6994e0819d67
bezlant <abezludniy@mail.ru>
2022-06-17 09:41:22 +0300

    Implemented better tests that actually test the functinality. (Before most tests were comparing zeroes with zeroes) DEBUG PRINTS PLS

--- src/tests/math_tests/test_s21_div.c
+++ src/tests/math_tests/test_s21_div.c
@@ -2,41 +2,75 @@
 
 START_TEST(gcc_128_bits) {
     long long long_a = get_random_ll() * rand();
-    long long long_b = get_random_ll() * rand();
-    // printf("long_a  =%lld\n", long_a);
-    // printf("long_b  =%lld\n", long_b);
+    long long long_b = get_random_ll();
+
+    if (rand() % 2)
+        long_a *= -1;
+
+    if (rand() % 2)
+        long_b *= -1;
+
+    printf("long_a = %lld\n", long_a);
+    printf("long_b = %lld\n", long_b);
+
     __int128_t a = long_a;
     __int128_t b = long_b;
-    __int128_t div = a / b;
-    s21_decimal res128 = bigint_to_decimal(div);
+    __int128_t divided = a / b;
+    printf("divided = %lld\n", (long long)divided);
+
+    s21_decimal res128 = bigint_to_decimal(divided);
 
     s21_decimal dec_a = ll_to_decimal(long_a);
-
-    // print_bits_r(dec_a);
     s21_decimal dec_b = ll_to_decimal(long_b);
     s21_decimal dec_div = {0};
 
-    // print_bits_r(dec_b);
-
     int code = s21_div(dec_a, dec_b, &dec_div);
 
-    // printf("dec_div=");
-    // print_bits_r(dec_div);
-    // printf("res128_=");
-    // print_bits_r(res128);
+    printf("dec_div=");
+    print_bits_r(dec_div);
+    printf("res128_=");
+    print_bits_r(res128);
 
     int comp_res = s21_is_equal(res128, dec_div);
 
-    if (!comp_res) {
-        /* I suspect that it is not our problem that signs do not match here */
-        /* GCC __int128_t are unsigned, thus, they discard sign */
-        /* In observed failed tests our decimal had sign & int 128 didn't */
-        printf(
-            GRN
-            "WARNING! SIGNS DO NOT MATCH! COMPARING ABS VALUES 📌 \n" ENDCOLOR);
-        set_sign_pos(&dec_div);
-        comp_res = s21_is_equal(res128, dec_div);
-    }
+    ck_assert_int_eq(code, ARITHMETIC_OK);
+    ck_assert_int_eq(comp_res, TRUE);
+}
+
+START_TEST(random_float) {
+    float float_a = get_random_float(-100.123456, 100.123456);
+
+    float float_b = get_random_float(-10.123456, 10.123456);
+
+    if (rand() % 2)
+        float_a *= -1;
+
+    if (rand() % 2)
+        float_b *= -1;
+
+    printf("float_a = %f\n", float_a);
+    printf("float_b = %f\n", float_b);
+    float float_res = float_a / float_b;
+    printf("divided = %f\n", float_res);
+
+    s21_decimal res_float = {0};
+    s21_from_float_to_decimal(float_res, &res_float);
+
+    s21_decimal dec_a = {0};
+    s21_from_float_to_decimal(float_a, &dec_a);
+    s21_decimal dec_b = {0};
+    s21_from_float_to_decimal(float_b, &dec_b);
+
+    s21_decimal dec_div = {0};
+
+    int code = s21_div(dec_a, dec_b, &dec_div);
+
+    printf("dec_div=");
+    print_bits_r(dec_div);
+    printf("res_float =");
+    print_bits_r(res_float);
+
+    int comp_res = s21_is_equal(res_float, dec_div);
 
     ck_assert_int_eq(code, ARITHMETIC_OK);
     ck_assert_int_eq(comp_res, TRUE);
@@ -160,14 +194,16 @@ Suite *suite_s21_div(void) {
     TCase *tc = tcase_create("s21_div_tc");
 
     /* ✅ Heavily tested. Passed all 40000 tests several times */
-    tcase_add_loop_test(tc, divison_by_one, 0, 100);
-    tcase_add_loop_test(tc, divison_by_two, 0, 100);
-    tcase_add_loop_test(tc, gcc_128_division_by_ten, 0, 100);
-    tcase_add_loop_test(tc, gcc_128_bits, 0, 100);
-    tcase_add_loop_test(tc, divison_by_rand_int, 0, 100);
+    /* tcase_add_loop_test(tc, divison_by_one, 0, 100); */
+    /* tcase_add_loop_test(tc, divison_by_two, 0, 100); */
+    /* tcase_add_loop_test(tc, gcc_128_division_by_ten, 0, 100); */
+    /* tcase_add_loop_test(tc, gcc_128_bits, 0, 1); */
+    tcase_add_loop_test(tc, random_float, 0, 1);
+
+    /* tcase_add_loop_test(tc, divison_by_rand_int, 0, 100); */
 
     /* 🌃 Edge cases */
-    tcase_add_loop_test(tc, division_by_zero_and_zero_like_vals, 0, 100);
+    /* tcase_add_loop_test(tc, division_by_zero_and_zero_like_vals, 0, 100); */
 
     /**
      * [#38] TODO: (!) - SEEN BY @bezlant WILL WORK ON IT TOMORROW (14.06)

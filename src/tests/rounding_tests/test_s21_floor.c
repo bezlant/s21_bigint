1ac9b022d1a2a8e73e0944429c39ea362e02380a
bezlant <abezludniy@mail.ru>
2022-06-17 15:27:31 +0300

    fixed tests

--- src/tests/rounding_tests/test_s21_floor.c
+++ src/tests/rounding_tests/test_s21_floor.c
@@ -2,12 +2,19 @@
 
 START_TEST(gcc_128_bits) {
     /* get random value */
-    __int128_t a = llabs(get_random_ll() * rand());
+    __int128_t a = get_random_ll() * rand();
+    if (rand() % 2)
+        a = -a;
 
     /* discard fraction & return it with zeroes*/
     int exp = get_rand(3, 10);
     __int128_t b = int128_pow(10, exp);
-    __int128_t sum = a / b * b;
+    __int128_t sum = a / b;
+
+    if (sum < 0)
+        sum -= 1;
+
+    sum *= b;
 
     /* get expected */
     s21_decimal expected = bigint_to_decimal(sum);
@@ -15,14 +22,13 @@ START_TEST(gcc_128_bits) {
 
     s21_decimal val = bigint_to_decimal(a);
     set_exponent(&val, exp);
+
     s21_decimal got = {0};
 
-    s21_decimal tmp = val;
-    set_sign_neg(&tmp);
-    (void)s21_floor(tmp, &got);
     int ret = s21_floor(val, &got);
 
 #ifdef DEBUG
+    printf("exponenet = %d\n", get_exponent(val));
     printf("val=     ");
     print_bits_r(val);
     printf("expected=");
@@ -40,7 +46,7 @@ Suite *suite_s21_floor(void) {
     Suite *s = suite_create(PRETTY_PRINT("s21_floor"));
     TCase *tc = tcase_create("s21_floor_tc");
 
-    tcase_add_loop_test(tc, gcc_128_bits, 0, 100);
+    tcase_add_loop_test(tc, gcc_128_bits, 0, 100000);
 
     suite_add_tcase(s, tc);
     return s;

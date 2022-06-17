35bab8d1aedd4061401d95cfd75848036ecbef8b
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-06-17 14:00:48 +0300

    Hotfix #67 (div inf loop)

--- src/tests/math_tests/test_s21_div.c
+++ src/tests/math_tests/test_s21_div.c
@@ -1,5 +1,4 @@
 #include "../s21_decimal_test.h"
-#define DEBUG
 
 START_TEST(gcc_128_bits) {
     long long long_a = get_random_ll() * rand();
@@ -43,48 +42,6 @@ START_TEST(gcc_128_bits) {
     ck_assert_int_eq(comp_res, TRUE);
 }
 
-START_TEST(random_float) {
-    float float_a = get_random_float(-100.123456, 100.123456);
-
-    float float_b = get_random_float(-10.123456, 10.123456);
-
-    if (rand() % 2)
-        float_a *= -1;
-
-    if (rand() % 2)
-        float_b *= -1;
-
-    float float_res = float_a / float_b;
-#ifdef DEBUG
-    printf("float_a = %f\n", float_a);
-    printf("float_b = %f\n", float_b);
-    printf("divided = %f\n", float_res);
-#endif
-
-    s21_decimal res_float = {0};
-    s21_from_float_to_decimal(float_res, &res_float);
-
-    s21_decimal dec_a = {0};
-    s21_from_float_to_decimal(float_a, &dec_a);
-    s21_decimal dec_b = {0};
-    s21_from_float_to_decimal(float_b, &dec_b);
-
-    s21_decimal dec_div = {0};
-
-    int code = s21_div(dec_a, dec_b, &dec_div);
-
-#ifdef DEBUG
-    printf("dec_div=");
-    print_bits_r(dec_div);
-    printf("res_float =");
-    print_bits_r(res_float);
-#endif
-
-    int comp_res = s21_is_equal(res_float, dec_div);
-
-    ck_assert_int_eq(code, ARITHMETIC_OK);
-    ck_assert_int_eq(comp_res, TRUE);
-}
 
 START_TEST(gcc_128_division_by_ten) {
     long long long_a = get_random_ll() * rand();
@@ -174,9 +131,101 @@ START_TEST(division_by_zero_and_zero_like_vals) {
 }
 END_TEST
 
+START_TEST(gcc_128_bits_negative) {
+    long long long_a = get_random_ll() * rand();
+    long long long_b = get_random_ll();
+
+    if (rand() % 2)
+        long_a = -long_a;
+
+    if (rand() % 2)
+        long_b = -long_b;
+
+#ifdef DEBUG
+    printf("long_a = %lld\n", long_a);
+    printf("long_b = %lld\n", long_b);
+#endif
+
+    __int128_t a = long_a;
+    __int128_t b = long_b;
+    __int128_t divided = a / b;
+
+#ifdef DEBUG
+    printf("divided = %lld\n", (long long)divided);
+#endif
+
+    s21_decimal res128 = bigint_to_decimal(divided);
+
+    s21_decimal dec_a = ll_to_decimal(long_a);
+    s21_decimal dec_b = ll_to_decimal(long_b);
+    s21_decimal dec_div = {0};
+
+    int code = s21_div(dec_a, dec_b, &dec_div);
+
+#ifdef DEBUG
+    printf("dec_div=");
+    print_bits_r(dec_div);
+    printf("res128_=");
+    print_bits_r(res128);
+#endif
+
+    int comp_res = s21_is_equal(res128, dec_div);
+
+    ck_assert_int_eq(code, ARITHMETIC_OK);
+    ck_assert_int_eq(comp_res, TRUE);
+}
+
+START_TEST(random_float) {
+    float float_a = get_random_float(-100.123456, 100.123456);
+
+    float float_b = get_random_float(-10.123456, 10.123456);
+
+    if (rand() % 2)
+        float_a *= -1;
+
+    if (rand() % 2)
+        float_b *= -1;
+
+    float float_res = float_a / float_b;
+
+#define DEBUG
+
+#ifdef DEBUG
+    printf("float_a = %f\n", float_a);
+    printf("float_b = %f\n", float_b);
+    printf("divided = %f\n", float_res);
+#endif
+
+    s21_decimal res_float = {0};
+    s21_from_float_to_decimal(float_res, &res_float);
+
+    s21_decimal dec_a = {0};
+    s21_from_float_to_decimal(float_a, &dec_a);
+    s21_decimal dec_b = {0};
+    s21_from_float_to_decimal(float_b, &dec_b);
+
+    s21_decimal dec_div = {0};
+
+    int code = s21_div(dec_a, dec_b, &dec_div);
+
+#ifdef DEBUG
+    printf("dec_div=");
+    print_bits_r(dec_div);
+    printf("res_float =");
+    print_bits_r(res_float);
+#endif
+
+    int comp_res = s21_is_equal(res_float, dec_div);
+
+    ck_assert_int_eq(code, ARITHMETIC_OK);
+    ck_assert_int_eq(comp_res, TRUE);
+}
+
 Suite *suite_s21_div(void) {
     Suite *s = suite_create(PRETTY_PRINT("s21_div"));
     TCase *tc = tcase_create("s21_div_tc");
+
+    // tcase_add_loop_test(tc, gcc_128_bits_negative, 0, 100);
 
     /* ✅ Heavily tested. Passed all 40000 tests several times */
     /* tcase_add_loop_test(tc, divison_by_one, 0, 100); */

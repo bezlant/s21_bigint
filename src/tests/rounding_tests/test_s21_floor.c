ae84e5d8c62a5ed467fdaa8f13a9decd65c84fa6
bezlant <abezludniy@mail.ru>
2022-06-23 17:02:47 +0300

    Simplified & Fixed rounders. Tests are fully functional.

--- src/tests/rounding_tests/test_s21_floor.c
+++ src/tests/rounding_tests/test_s21_floor.c
@@ -1,42 +1,47 @@
 #include "../s21_decimal_test.h"
 
-START_TEST(gcc_128_bits) {
-    /* get random value */ __int128_t a = get_random_ll() * rand();
+START_TEST(random_loop) {
+    int x = rand();
+
     if (rand() % 2)
-        a = -a;
-
-    /* discard fraction & return it with zeroes*/
-    int exp = get_rand(3, 10);
-    __int128_t b = int128_pow(10, exp);
-    __int128_t sum = a / b;
-
-    if (sum < 0)
-        sum -= 1;
-
-    sum *= b;
-
-    /* get expected */
-    s21_decimal expected = bigint_to_decimal(sum);
-    set_exponent(&expected, exp);
-
-    s21_decimal val = bigint_to_decimal(a);
-    set_exponent(&val, exp);
-
-    s21_decimal got = {0};
-
-    int ret = s21_floor(val, &got);
+        x = -x;
 
 #ifdef DEBUG
-    printf("exponent = %d\n", get_exponent(val));
-    printf("val=     ");
-    print_bits_r(val);
-    printf("expected=");
-    print_bits_r(expected);
-    printf("got     =");
-    print_bits_r(got);
+    static int test = 0;
+    printf("----------------------------------------\n");
+    printf("test %d\n", test++);
+    printf("rand x =      %d\n", x);
 #endif
 
-    ck_assert_int_eq(s21_is_equal(expected, got), TRUE);
+    s21_decimal expected = {0};
+    int expected_int = abs(x);
+
+    if (x < 0 && expected_int % 10 != 0)
+        expected_int = (expected_int / 10 + 1);
+    else
+        expected_int /= 10;
+
+    expected_int = x < 0 ? -expected_int : expected_int;
+
+    s21_from_int_to_decimal(expected_int, &expected);
+
+    s21_decimal got = {0};
+    s21_from_int_to_decimal(x, &got);
+    set_exponent(&got, 1);
+
+    s21_decimal res = {0};
+    int ret = s21_floor(got, &res);
+
+#ifdef DEBUG
+    printf("expected_int = %d\n", expected_int);
+    printf("expected = ");
+    print_bits_r(expected);
+    printf("res      = ");
+    print_bits_r(res);
+    printf("----------------------------------------\n");
+#endif
+
+    ck_assert_int_eq(s21_is_equal(expected, res), TRUE);
     ck_assert_int_eq(ret, CONVERTATION_OK);
 }
 END_TEST
@@ -45,7 +50,7 @@ Suite *suite_s21_floor(void) {
     Suite *s = suite_create(PRETTY_PRINT("s21_floor"));
     TCase *tc = tcase_create("s21_floor_tc");
 
-    tcase_add_loop_test(tc, gcc_128_bits, 0, 1000);
+    tcase_add_loop_test(tc, random_loop, 0, 1000);
 
     suite_add_tcase(s, tc);
     return s;
